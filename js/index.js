7962edafb66b22c521027401aa01bca7988ade5a
Sam K <kolhar730@gmail.com>
2017-09-10 22:55:59 +0530

    All Files added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+(function() {
+    var config = {
+        apiKey: "AIzaSyDQMG0hEcQIKirnzIp_-QODnlrxdilhLHc",
+        authDomain: "polymer-fire-trial.firebaseapp.com",
+        databaseURL: "https://polymer-fire-trial.firebaseio.com",
+        projectId: "polymer-fire-trial",
+        storageBucket: "polymer-fire-trial.appspot.com",
+        messagingSenderId: "928034571785"
+    };
+
+    firebase.initializeApp(config);
+}())
